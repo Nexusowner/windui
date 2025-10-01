854d72f5a5b9d52eacbf7bd29e8e7e4d39660dae
developed <Nexus.owner@yahoo.com>
2025-10-01 19:10:18 +0100

    Update windui.lua

--- windui.lua
+++ windui.lua
@@ -5,15 +5,17 @@ local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footag
 WindUI:SetTheme("Dark")
 WindUI.TransparencyValue = 0.2
 
-local Window = WindUI:CreateWindow({
-    Title = "Spawner UI",
-    Author = "By Nexus Developed",
-    Icon = "magic",
-    Size = UDim2.fromOffset(580, 490),
-    Folder = "NexusHub",
-Background = "rbxassetid://90045700186596"
-})
+local Window = nil
 
+function Library:Setup()
+    Window = WindUI:CreateWindow({
+        Title = "Spawner UI",
+        Author = "By Nexus Developed",
+        Icon = "magic",
+        Size = UDim2.fromOffset(580, 490),
+        Folder = "NexusHub",
+        Background = "rbxassetid://90045700186596"
+    })
     return Window
 end
 
